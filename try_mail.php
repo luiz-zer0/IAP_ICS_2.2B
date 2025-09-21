eaafc290a15c52b1b06a49bd767ab1df5747377f
luiz <louisngatia47@gmail.com>
2025-09-21 17:18:15 +0300

    randomizing your acivation code in the email

--- try_mail.php
+++ try_mail.php
@@ -1,36 +1,22 @@
 <?php
-require 'ClassAutoLoad.php';
+require 'classAutoLoad.php';
 
-$name='User';
-$email='';
+$mailCnt= [
+    'from_name' => 'sender name',
+    'from_email' => 'sender@yahpoo.com',
+    'to_name' => 'receiver name',
+    'to_email' => 'recipient@gmail.com',
+    'subject' => 'Hello from ICS B',
+    'body' => '<h1>This is a test email</h1><p>This is the body of the email.</p>'
+];
+
+// $ObjSendMail->Send_Mail($conf,$mailCnt);
 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Process form data
-    $name=$_POST['name'] ?? 'User';
-    $email=$_POST['email'] ?? '';
-    $password=$_POST['password'] ?? '';
-}
+print rand(100000,999999);
 
 
-  if (!empty($email)) {
-        $mailCnt = [
-            'from_name' => 'Admin',
-            'from_email' => 'Louis.Muturi@strathmore.edu',
-            'to_name' => $name,
-            'to_email' => $email,
-            'subject' => 'Welcome to ICS 2.2! Account verification',
-            'body' => "Hello $name, <br> You requested an account on ICS 2.2.<br><br>In order to use this account, Please <a href=''>click here</a> to complete the registration process.<br><br> If you did not request this account, please ignore this email.<br><br> Regards,<br> System Admin<br> ICS 2.2"
-        ];
 
-        $ObjSendMail = new SendMail();
-        $ObjSendMail->Send_Mail($conf, $mailCnt);
-    }
 
-       // Display result message
-        echo "<h2>Thank you for signing up, $name!</h2>";
-        echo "<p>A verification email has been sent to $email. Please check your inbox.</p>";
-        exit(); // Stop execution after sending email
-    
 
 ?>
