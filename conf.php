29b4aa8d83a601cec895881b3795ffc50d6badcd
luiz <louisngatia47@gmail.com>
2025-09-21 17:20:19 +0300

    adding the session in the configuration

--- conf.php
+++ conf.php
@@ -1,12 +1,18 @@
 <?php
 
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+//site timezone
+$conf['site_timezone']='Africa/Nairobi';
 //site info
 $conf['site_name']='ICS B';
-$conf['site_url']='http://loclahost/tol';
+$conf['site_url']='http://localhost:8080';
 $conf['admin_email']='admin@isacademy.com';
 
 //site language
 $conf['site_lang']='en';
+require __DIR__.'/lang/'.$conf['site_lang'].'.php';
 
 //database connection
 $conf['db_type']='pdo';
@@ -22,4 +28,17 @@ $conf['smtp_user']='Louis.Muturi@strathmore.edu';
 $conf['smtp_pass']='rtga bzpy jilm uzgo';
 $conf['smtp_port']=465;
 $conf['smtp_secure']='ssl'; //tls or ssl
+
+// Set password length
+$conf['min_password_length'] = 8;
+
+// Set valid email domain (for example: 'icsbacademy.com')
+$conf['valid_email_domain'] = ['icsbacademy.com', 'yahoo.com', 'gmail.com', 'outlook.com', 'hotmail.com', 'strathmore.edu'];
+
+//setting random verification code 
+$conf['reg_ver_code']=rand(100000,999999);
+
+// Set verification code expiry time (in minutes)
+$conf['ver_code_expiry'] = 5;
+
 ?>
